6a359dde1c9e2fbb2904d4bcd39e2f32ec555e8b
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-24 14:27:50 -0400

    search works

--- components/JBProjectDropdown.tsx
+++ components/JBProjectDropdown.tsx
@@ -1,39 +1,65 @@
 import type { NextPage } from "next";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Image from "next/image";
 import Head from "next/head";
-import { useState } from "react";
-
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { Combobox } from "@headlessui/react";
+import _ from "lodash";
 
-const people = [
-  { id: 1, name: "Leslie Alexander" },
-  // More users...
-];
+interface Project {
+  id: string;
+  name: string;
+  [key: string]: any; // Additional properties not listed here
+}
 
-function classNames(...classes: (string | boolean)[]) {
+function classNames(...classes: (false | null | undefined | string)[]) {
   return classes.filter(Boolean).join(" ");
 }
 
 const JBProjectDropdown: FC = () => {
   const [query, setQuery] = useState("");
-  const [selectedPerson, setSelectedPerson] = useState(null);
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+  
+  const [projects, setProjects] = useState<Project[]>([]);
 
-  const filteredPeople =
+  useEffect(() => {
+    console.log(selectedProject);
+  }, [selectedProject]);
+
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      const response = await fetch(
+        `https://juicebox.money/api/projects?text=${query}`
+      );
+      const data: Project[] = await response.json();
+      setProjects(data.slice(0, 5));
+    };
+
+    if (query !== "") {
+      const debouncedFetch = _.debounce(fetchProjects, 250);
+      debouncedFetch();
+    } else {
+      setProjects([]);
+    }
+  }, [query]);
+
+  const filteredProjects =
     query === ""
-      ? people
-      : people.filter((person) => {
-          return person.name.toLowerCase().includes(query.toLowerCase());
+      ? []
+      : projects.filter((project) => {
+          return project.name.toLowerCase().includes(query.toLowerCase());
         });
 
   return (
-    <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson}>
+    <Combobox as="div" value={selectedProject} onChange={setSelectedProject}>
       <div className="relative my-2 max-w-sm ">
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(person) => person?.name}
+          displayValue={(project: Project | null) =>
+            project?.name || "Juicebox"
+          }
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon
@@ -42,12 +68,12 @@ const JBProjectDropdown: FC = () => {
           />
         </Combobox.Button>
 
-        {filteredPeople.length > 0 && (
+        {filteredProjects.length > 0 && (
           <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredPeople.map((person) => (
+            {filteredProjects.map((project) => (
               <Combobox.Option
-                key={person.id}
-                value={person}
+                key={project.id}
+                value={project}
                 className={({ active }) =>
                   classNames(
                     "relative cursor-default select-none py-2 pl-8 pr-4",
@@ -63,7 +89,7 @@ const JBProjectDropdown: FC = () => {
                         selected && "font-semibold"
                       )}
                     >
-                      {person.name}
+                      {project.name}{" "}
                     </span>
 
                     {selected && (
