60d90eb9bc53d62eebc9aea09bbcd08021a37758
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-23 20:17:10 -0400

    changes

---
+++ api/getKey.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(_: NextApiRequest, res: NextApiResponse) {
+    if (!process.env.API_KEY) {
+        return res.status(500).json({ error: 'API Key not configured in the environment variables' });
+    }
+
+    res.status(200).json({ key: process.env.API_KEY });
+}
