6b4279dab22cdba3ac27b119cf7b59db3a53efda
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-24 21:59:07 -0400

    added stuff idk what

--- components/MintModule.tsx
+++ components/MintModule.tsx
@@ -2,37 +2,41 @@ import type { NextPage } from "next";
 import { FC } from "react";
 import Image from "next/image";
 import Head from "next/head";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MintButton from "./MintButton";
 import Search from "./Search";
 import Preview from "./Preview";
+import { Project } from "../interfaces/Project";
 
 type MintModuleProps = {
   className?: string;
 };
 
 const MintModule: FC<MintModuleProps> = ({ className = "" }) => {
-    const [selectedProject, setSelectedProject] = useState<Project | null>({
-      id: "1",
-      name: "Juicebox",
-    });
-  
-    return (
-      <div className={`${className}`}>
-        <h2>Mint a Project Card</h2>
-        <div className="text-gray-400 mt-2">Choose any Juicebox project.</div>
-        <Search selectedProject={selectedProject} setSelectedProject={setSelectedProject} />
-        <Preview selectedProject={selectedProject} />
-        <MintButton />
-        <div className=" mt-2 text-gray-300 text-xs">
-          Proceeds go to the{" "}
-          <a href="https://juicebox.money/v2/p/465">
-            <span className="italic underline">metadata</span>
-          </a>{" "}
-          project.
-        </div>
+  const [selectedProject, setSelectedProject] = useState<Project | null>({
+    id: "1",
+    name: "Juicebox",
+  });
+
+  return (
+    <div className={`${className}`}>
+      <h2>Mint a Project Card</h2>
+      <div className="text-gray-400 mt-2">Choose any Juicebox project.</div>
+      <Search
+        selectedProject={selectedProject}
+        setSelectedProject={setSelectedProject}
+      />
+      <Preview selectedProject={selectedProject} />
+      <MintButton />
+      <div className=" mt-2 text-gray-300 text-xs">
+        Proceeds go to the{" "}
+        <a href="https://juicebox.money/v2/p/465">
+          <span className="italic underline">metadata</span>
+        </a>{" "}
+        project.
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default MintModule;
--- components/Preview.tsx
+++ components/Preview.tsx
@@ -3,13 +3,32 @@ import { FC } from "react";
 import Image from "next/image";
 import { useState } from "react";
 import { Project } from "../interfaces/Project";
+import { mainnet } from "wagmi/chains";
+import { useJbProjectsTokenUri } from "../resources/generated";
 
 interface PreviewProps {
   selectedProject: Project | null;
 }
 
 const Preview: FC<PreviewProps> = ({ selectedProject }) => {
-  return <></>;
+  const tokenId = selectedProject ? BigInt(selectedProject.id) : undefined;
+
+  const {
+    data: tokenUri,
+    status,
+    error,
+  } = useJbProjectsTokenUri({
+    args: tokenId ? [tokenId] : undefined,
+  });
+
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'error') return <p>Error: {error?.message}</p>;
+
+  return (
+    <div>
+      {tokenUri && <p>Token URI: {tokenUri}</p>}
+    </div>
+  );
 };
 
 export default Preview;
