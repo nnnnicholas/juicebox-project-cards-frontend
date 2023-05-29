adab5c616ddf05e769faaa95fc8fedfd769d0370
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-29 11:17:03 -0400

    added cacheing to top nfts lookup

--- pages/api/topnfts.ts
+++ pages/api/topnfts.ts
@@ -17,18 +17,32 @@ const options = {
 // Define the contract address constant
 const JUICEBOX_PROJECT_CARDS_ADDRESS = "0xe601Eae33A0109147a6F3CD5F81997233d42FEDD";
 
+// Cache duration in milliseconds
+const CACHE_DURATION = 1000 * 120; // 2 minutes
+
+// Cache data and timestamp
+let cache: { timestamp: number; data: any } = { timestamp: 0, data: null };
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await alchemy.nft.getNftsForContract(JUICEBOX_PROJECT_CARDS_ADDRESS, options);
+    const currentTime = new Date().getTime();
+    const timeDiff = currentTime - cache.timestamp;
 
-    // Get mint counts for each NFT
-    const mintCountsPromises = response.nfts.map((nft) => alchemy.nft.getOwnersForNft(JUICEBOX_PROJECT_CARDS_ADDRESS, nft.tokenId));
-    const mintCounts = await Promise.all(mintCountsPromises);
+    if (timeDiff > CACHE_DURATION || !cache.data) {
+      const response = await alchemy.nft.getNftsForContract(JUICEBOX_PROJECT_CARDS_ADDRESS, options);
 
-    // Build response with mint counts
-    const nfts = response.nfts.map((nft, index) => ({ tokenId: nft.tokenId, mintCount: mintCounts[index].owners.length }));
+      // Get mint counts for each NFT
+      const mintCountsPromises = response.nfts.map((nft) => alchemy.nft.getOwnersForNft(JUICEBOX_PROJECT_CARDS_ADDRESS, nft.tokenId));
+      const mintCounts = await Promise.all(mintCountsPromises);
 
-    res.status(200).json({ nfts });
+      // Build response with mint counts
+      const nfts = response.nfts.map((nft, index) => ({ tokenId: nft.tokenId, mintCount: mintCounts[index].owners.length }));
+
+      // Update cache
+      cache = { timestamp: currentTime, data: { nfts } };
+    }
+
+    res.status(200).json(cache.data);
   } catch (err) {
     // Assert `err` to be of type `Error`
     const error = err as Error;
