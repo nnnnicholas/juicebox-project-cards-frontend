4a88b68eacd0ab40d15f3422a3eb6e406940f255
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-29 20:35:19 -0400

    fixed card alignment

--- components/TopNfts.tsx
+++ components/TopNfts.tsx
@@ -26,11 +26,18 @@ const TopNftsComponent: FC = () => {
     <div className="grow">
       <h1>Most popular</h1>
       <p className="text-gray-500 mt-2">
-        The top three most minted Juicebox Cards.
+        The top three most minted Juicebox Cards. See the full collection on{" "}
+        <a
+          className="underline"
+          href="https://opensea.io/collection/juicebox-project-cards"
+        >
+          OpenSea
+        </a>
+        .
       </p>
       <div
         id="cardContainer"
-        className="flex flex-col pt-8  items-center lg:items-baseline lg:flex-row lg:justify-space-between lg:place-content-between mt-4"
+        className="flex flex-col pt-8  items-center lg:items-stretch lg:flex-row lg:justify-space-between lg:place-content-between mt-4"
       >
         {topNfts.map(({ tokenId, mintCount }, index) => {
           const project: Project = {
@@ -40,10 +47,10 @@ const TopNftsComponent: FC = () => {
           return (
             <div
               key={index}
-              className="flex flex-col min-w-[300px] max-w-[340px] mb-8 mx-2 my-0 border-solid border-2 bg-gray-100 rounded-lg border-grey-600"
+              className="flex flex-col justify-end min-w-[300px] max-w-[340px] mb-8 mx-2 my-0 border-solid border-2 bg-gray-100 rounded-lg border-grey-600"
             >
-              <Preview selectedProject={project} className="w-full pb-4" />
-              <div className="flex flex-col ">
+              <Preview selectedProject={project} className="grow w-full flex items-center pb-4" />
+              <div className=" flex flex-col ">
                 <div className="flex w-full place-content-between items-baseline">
                   <h3 className="pl-4">{ranks[index]}</h3>
                   <div className="pr-4">{mintCount} minted</div>
